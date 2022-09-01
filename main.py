6831e4a99a873b0f6833245ec03fa00c689ba39f
Dmitriy Kurzykin <ametek84@mail.ru>
2022-09-01 16:27:32 +0300

    Not work, fucking shit

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+import requests
+
+
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+    def get_headers(self):
+        return {'Content-Type': 'application/json',
+                'Authorization': 'OAuth {}'.format(self.token)}
+
+    def _get_upload_link(self, path_to_file):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': path_to_file, "overwrite": "False"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload_file(self, path_to_file: str, filename: str):
+        href = self._get_upload_link(path_to_file=path_to_file).get('href', "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Upload success')
+
 
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
+    # Получаем путь к загружаемому файлу и токен от пользователя
+    path_to_file = input('Введите путь к загружаемому файлу: ')
+    token = input('Введите токен: ')
     uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    result = uploader.upload_file(path_to_file, filename)
